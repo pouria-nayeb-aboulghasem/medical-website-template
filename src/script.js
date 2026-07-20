017bc3397d175c5a14f3b3d595e37b7575e418cb
pour68 <pour68@outlook.com>
2026-07-20 14:34:09 +0330

    add dark theme

--- src/script.js
+++ src/script.js
@@ -9,3 +9,38 @@ closeMobileMenu.addEventListener("click", () => {
 openMobileMenu.addEventListener("click", () => {
     menu.classList.add("active");
 });
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 24,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    }
+});
+
+// active links
+
+const sections = document.querySelectorAll('[data-section]');
+
+window.addEventListener('scroll', () => {
+    let scrollDown = window.scrollY;
+
+    sections.forEach(current => {
+        let sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector(`#menu a[href*=${sectionId}]`);
+
+        if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+            sectionsClass.classList.add('active-link')
+        } else {
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+});
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    darkMode: "class",
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
     plugins: [
         tailwindcss(),
     ],
